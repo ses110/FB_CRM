5c79da3223c539df83caded0eff474b39890a570
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-11-16 13:37:01 -0600

    crazy shit

--- js/fbprm.js
+++ js/fbprm.js
@@ -2,6 +2,79 @@ var app = angular.module("fbprm", []);
 
 function fbprm($scope) {
 
+console.log('variables Initializing')
+	//Initializing variables
+	$scope.user = Parse.User.current();
+	//$scope.threads;
+
+	//preset threads
+	$scope.threads = [
+		{snippet: "snippet1", participants: "bob1", stage: "new", thread: "fskfjahf", id: "davefontenot", proPic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1117432_1725380113_505551893_q.jpg"},
+		{snippet: "snippet2", participants: "bob2", stage: "new", thread: "fskfjahf", id: "davefontenot", proPic: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1117432_1725380113_505551893_q.jpg"}
+	]
+
+	$scope.updatePics = function() {
+		for (i=0; i < $scope.threads.length; i++) {
+			$scope.threads[i].proPic = $scope.getProfilePic($scope.threads[i].id);
+		}
+		//$scope.threads[0].proPic = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1116314_629564354_324251643_q.jpg";
+	}
+
+	console.log($scope.threads);
+
+	//Preset values
+	$scope.name = ["Laura Ranola"];
+	$scope.snippet = ["Hi, I have a problem with the preorder button"];
+
+	console.log('variables Initialized')
+
+	$scope.grabThreads = function () {
+        var Thread = Parse.Object.extend("Threads");
+        var query = new Parse.Query(Thread);
+        //Find all posts by the current user
+        query.equalTo("user", $scope.user);
+        query.limit('3');
+        query.find({
+          success: function(usersThreads) {
+            $scope.threads = usersThreads;
+            console.log('loaded threads');
+            console.log($scope.threads);
+          }
+        });
+	}
+
+	$scope.updateThreads = function () {
+		//$scope.grabThreads();
+		//console.log("can push them onto array here");
+		//console.log($scope.threads);
+		for (i=0; i < 3; i++) {
+			console.log($scope.threads[i]);
+			console.log($scope.getSnippetForThread($scope.threads[i]));
+			$scope.threads.push($scope.threads[i]);
+		}
+	}
+
+	$scope.getThreadAtIndex = function (index) {
+        //saving object with threads to parse
+        return $scope.threads[index];
+    }
+
+    $scope.getAttributeForThreadAtIndex = function (attr, index) {
+    	//first get that thread, then find attribute...might have to do some switch cases
+    	console.log($scope.getThreadAtIndex(index))
+    }
+
+    $scope.getSnippetForThread = function (thread) {
+    	return thread.get('snippet');
+    }
+
+    //loads threads into $scope.threads
+    $scope.grabThreads();
+    console.log('loading threads');
+
+    //Get snippet for threadAtIndex n = 9
+    //console.log($scope.getSnippetForThread($scope.getThreadAtIndex($scopes.threads, 9)));
+
 	$scope.login = function () {
 		console.log('blah');
         Parse.FacebookUtils.logIn("read_mailbox", {
@@ -26,19 +99,26 @@ function fbprm($scope) {
             var threadsAvailable;
 
             //saving object with threads to parse
-            var user = Parse.User.current();
 
 
             // Save threads one by one
             for (i=0; i < response.threads.data.length; i++) {
+
+           	  //Grab and Set profile pic -- doing this elsewhere
+
               //Initialize user thread object
               var Thread = Parse.Object.extend("Threads");
               var thread = new Thread();  
-              thread.set("user", user);
+
+              //set data
+              thread.set("user", $scope.user);
+              //console.log($scope.getProfilePic(_.toArray(response.threads.data[i].participants.data)[1].id));
+              //thread.set("proPic", $scope.getProfilePic(response.threads.data[i].participants.data.id));
               thread.set("thread", response.threads.data[i]);
               thread.set("participants", response.threads.data[i].participants);
               thread.set("snippet", response.threads.data[i].snippet);
               thread.set("updatedTime", response.threads.data[i].updatedTime);
+              thread.set("stage", "new");
               //threads.set("body", "This is some great content.");
               thread.save(null, {
                 success: function(thread) {
@@ -58,25 +138,39 @@ function fbprm($scope) {
           });  
        }
 
-       $scope.readThread = function (atIndex) {
-            //saving object with threads to parse
-            var user = Parse.User.current();
-            var Thread = Parse.Object.extend("Threads");
-            //Find all posts by the current user
-            var query = new Parse.Query(Thread);
-            query.equalTo("user", user);
-            query.find({
-              success: function(usersThreads) {
-                //threadsAvailable = usersThreads;
-                // userPosts contains all of the posts by the current user.
-                console.log(usersThreads[atIndex]);
-                console.log(usersThreads[atIndex].get('snippet'))
-                return usersThreads[atIndex].get('snippet');
-              }
-            });
+       //call to fill in profile pics with fbIDs
+       $scope.getProfilePic = function (fbID) {
+       	console.log('trying to get pic');
+       	var proPic = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1117432_1725380113_505551893_q.jpg";
+           	FB.api('/' + fbID + '?fields=id,name,picture', function(response) {
+           		console.log(response.picture.data.url);
+           		return proPic = response.picture.data.url;
+           	});
+        //return proPic;
        }
 
-       $scope.readThread(9);
+       $scope.proPict = function () {
+       		//console.log($scope.getProfilePic('davefontenot'));
+       		return "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1117432_1725380113_505551893_q.jpg";
+       }
+
+       $scope.changeStage = function (thread) {
+       		console.log('changed stage');
+       }
+
+       $scope.filterByStage = function (stage) {
+       		console.log('showing only threads with this stage');
+       }
+
+       $scope.getNextThreads = function (threadNumber) {
+       		//query.skip(25*threadNumber); -- infinite scroll?
+       		console.log('getting next page of threads');
+       }
+
+       //$scope.getProfilePic('yefim323');
+
+
+	   //$scope.readThread(9);
 
        //we should have a function that grabs all the threads and stores them in a $scope variable
        //we should then have a function that lets you grab a particular thread
